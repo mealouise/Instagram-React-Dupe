561791fe26af05bdcda83e29bd6f0336b0807d59
Emma Sheridan <emmasheridan@Emmas-MacBook-Air.local>
2020-02-16 20:11:40 +0000

    removed hard code and added object

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './style.css';
 import Thumbnail from '../Thumbnail/Thumbnail';
 
+const profileDetails = {
+    name:"Emma",
+    description: "username",
+    img:"https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-19/s150x150/43913090_191135048473787_8258812009550708736_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_ohc=W6ichF5La4IAX94zofD&oh=bb96c7b8d7676c55639251bdc999795b&oe=5EFA08DE"
+
+};
+
 const Sidebar = (props) => {
     return (<div className="sidebar">
         <Thumbnail 
-            title="mea_louise" 
-            description="Emma"
-            img="https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-19/s150x150/43913090_191135048473787_8258812009550708736_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_ohc=W6ichF5La4IAX94zofD&oh=bb96c7b8d7676c55639251bdc999795b&oe=5EFA08DE"
+            title={profileDetails.name} 
+            description={profileDetails.description}
+            img={profileDetails.img}
         />
     </div>)
 };
